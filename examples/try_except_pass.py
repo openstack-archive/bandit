38e45288d1f09441118127b7e6b61fc8c1fdfc99
Tim Kelsey <tim.kelsey@hp.com>
2015-07-08 15:08:08 +0100

    Adding test for Try, Except, Pass
    Ignoring errors is bad in general, you should at least log it. This
    is a security problem as increased exceptions from a service may
    indicate attempts to break/fuzz it.
    
    Change-Id: I8f6ad98a3f3dccb89d15f2e50beeaf4ce093323a

---
+++ examples/try_except_pass.py
@@ -0,0 +1,28 @@
+# bad
+try:
+	a = 1
+except:
+	pass
+
+
+# bad
+try:
+	a = 1
+except Exception:
+	pass
+
+
+# bad
+try:
+	a = 1
+except ZeroDivisionError:
+	pass
+except:
+	a = 2
+
+
+# good
+try:
+	a = 1
+except:
+	a = 2
