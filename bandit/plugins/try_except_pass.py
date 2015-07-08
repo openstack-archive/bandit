8730934c0017c4169560098e3b6c0af2f3eeae19
Tim Kelsey <tim.kelsey@hp.com>
2015-07-08 15:08:08 +0100

    Adding test for Try, Except, Pass
    Ignoring errors is bad in general, you should at least log it. This
    is a security problem as increased exceptions from a service may
    indicate attempts to break/fuzz it.
    
    Change-Id: I8f6ad98a3f3dccb89d15f2e50beeaf4ce093323a

---
+++ bandit/plugins/try_except_pass.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import ast
+
+import bandit
+from bandit.core.test_properties import *
+
+
+@checks('ExceptHandler')
+def try_except_pass(context):
+    node = context.node
+    if len(node.body) == 1:
+        if isinstance(node.body[0], ast.Pass):
+            return bandit.Issue(
+                severity=bandit.MEDIUM,
+                confidence=bandit.HIGH,
+                text=("Try, Except, Pass detected.")
+            )
