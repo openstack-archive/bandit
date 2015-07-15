3e24973f13088ea12d9e68a7938ee0c8532c1f5d
Tim Kelsey <tim.kelsey@hp.com>
2015-07-15 17:10:14 +0100

    Improving SQL Injection detection
    This patch replaces the noisy SQL injection test with two new
    more specific tests. These will catch a string that looks like
    SQL that has some form of binary opperation being performed.
    
    E.g.
    
    "select %s from derp;" % var
    "select thing from " + tab
    
    Would trigger a low confidence warning, the old test would warn
    on any string that looked like SQL, even if no operation was
    performed on it.
    
    A second more confident test checks for SQL like strings being
    using with a binop thats wrapped by a call to "execute" or
    "executemany", this is inline with the Python DBAPI, e.g
    
    cursor.execute("select %s from derp" % var)
    
    Will now give a medium confidence warning.
    
    The severity adjustment based on the presence of SQLAlchemy has
    been removed as importing the module does not alter the detected
    issue at all.
    
    Change-Id: I7bbb8bc5700146832227bc24d6c397b065ab569f
    Closes-Bug: 1422916

---
+++ examples/sql_statements.py
@@ -0,0 +1,23 @@
+import sqlalchemy
+
+# bad
+query = "SELECT * FROM foo WHERE id = '%s'" % identifier
+query = "INSERT INTO foo VALUES ('a', 'b', '%s')" % value
+query = "DELETE FROM foo WHERE id = '%s'" % identifier
+query = "UPDATE foo SET value = 'b' WHERE id = '%s'" % identifier
+
+# bad
+cur.execute("SELECT * FROM foo WHERE id = '%s'" % identifier)
+cur.execute("INSERT INTO foo VALUES ('a', 'b', '%s')" % value)
+cur.execute("DELETE FROM foo WHERE id = '%s'" % identifier)
+cur.execute("UPDATE foo SET value = 'b' WHERE id = '%s'" % identifier)
+
+# good
+cur.execute("SELECT * FROM foo WHERE id = '%s'", identifier)
+cur.execute("INSERT INTO foo VALUES ('a', 'b', '%s')", value)
+cur.execute("DELETE FROM foo WHERE id = '%s'", identifier)
+cur.execute("UPDATE foo SET value = 'b' WHERE id = '%s'", identifier)
+
+# real world false positives
+choices=[('server_list', _("Select from active instances"))]
+print("delete from the cache as the first argument")
