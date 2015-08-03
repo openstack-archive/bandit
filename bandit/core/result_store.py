eb6b6925bff3eabd059a5c6655b08af073871b3a
Travis McPeak <travis.mcpeak@hp.com>
2015-08-03 10:15:51 -0600

    Adding report timestamp
    This commit adds a timestamp to the report object.  Different
    formatters may choose to use it or not.  The datetime is provided
    as a datetime object, so it's up to the formatter to determine how
    to represent it.
    
    Change-Id: Ia921208dc9fec7516c567f6c5d7c63d538279a75

--- bandit/core/result_store.py
+++ bandit/core/result_store.py
@@ -18,6 +18,7 @@
 """An object to store/access results associated with Bandit tests."""
 
 from collections import OrderedDict
+import datetime
 import linecache
 
 from bandit.core import constants
@@ -41,6 +42,7 @@ class BanditResultStore():
         self.format = 'txt'
         self.out_file = None
         self.verbose = verbose
+        self.generated_time = _get_cur_datetime()
 
     def skip(self, filename, reason):
         '''Indicates that the specified file was skipped and why
@@ -124,6 +126,10 @@ class BanditResultStore():
         except IOError:
             print("Unable to write to file: %s" % self.out_file)
 
+    @property
+    def generated_time(self):
+        return self.generated_time
+
     def _get_issue_list(self):
 
         collector = list()
@@ -200,3 +206,13 @@ class BanditResultStore():
         :return: boolean result
         '''
         return constants.RANKING.index(severity) >= self.level
+
+
+def _get_cur_datetime():
+    try:
+        time_object = datetime.datetime.utcnow()
+    except OSError:
+        # catch the case where we aren't able to get the time
+        return None
+    else:
+        return time_object
