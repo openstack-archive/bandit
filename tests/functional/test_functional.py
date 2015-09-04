2aab22ba7143b110513c0577a43bb4c4f13b1047
Eric Brown <browne@vmware.com>
2015-09-04 13:50:45 -0700

    Check for insecure cipher modes
    ECB is a known insecure cipher mode and just be checked for.
    
    Change-Id: I27c30cb93e814eb3b3ce6732e94a76128b5b9e81

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -92,6 +92,11 @@ class FunctionalTests(testtools.TestCase):
         expect = {'SEVERITY': {'MEDIUM': 8}, 'CONFIDENCE': {'HIGH': 8}}
         self.check_example('crypto-md5.py', expect)
 
+    def test_cipher_modes(self):
+        '''Test for insecure cipher modes.'''
+        expect = {'SEVERITY': {'HIGH': 8}, 'CONFIDENCE': {'HIGH': 8}}
+        self.check_example('cipher-modes.py', expect)
+
     def test_eval(self):
         '''Test the `eval` example.'''
         expect = {'SEVERITY': {'MEDIUM': 3}, 'CONFIDENCE': {'HIGH': 3}}
