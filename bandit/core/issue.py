5761a5e9b564df61f4e73e5997b51fd79a139ee5
Travis McPeak <travis.mcpeak@hp.com>
2015-11-02 14:51:51 +0100

    Fixing Baseline when a filter is used
    This current baseline doesn't work if a filter is provided due
    to the details mentioned here:
      https://bugs.launchpad.net/bandit/+bug/1512360
    
    This fixes it by implementing the severity and confidence
    filtering before baseline filtering.
    
    Additionally, since each formatter used an IF statement to check
    severity after getting issues, we've just moved the filtering to
    the get_issues call.  If for some reason a formatter wants
    to get unfiltered, they can omit the call.
    
    Change-Id: I39c1d826f44a8c0d829fb7017d03dbff25779cba
    Closes-Bug: #1512360

--- bandit/core/issue.py
+++ bandit/core/issue.py
@@ -39,13 +39,16 @@ class Issue(object):
             self.confidence, self.fname, self.lineno)
 
     def filter(self, severity, confidence):
-        '''Used to filter on confidence and severity.
+        '''Utilty to filter on confidence and severity
 
-        This wil return false if either the confidence or severity of the issue
-        are lower then the given threashold values.
+        Formatters will call filter_results() directly.  This will return
+        false if either the confidence or severity of the issue are lower
+        than the given threshold values.
 
-        :param severity: Severity threashold
-        :param confidence: Confidence threashold
+        :param severity: Severity threshold
+        :param confidence: Confidence threshold
+        :return: True/False depending on whether issue meets threshold
+
         '''
         rank = constants.RANKING
         return (rank.index(self.severity) >= rank.index(severity) and
