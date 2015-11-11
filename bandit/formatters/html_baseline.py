9b3e5bc151cd72ce600a1a96df52aa8ded2582a9
Travis McPeak <travis.mcpeak@hp.com>
2015-11-11 19:03:26 +0100

    Adding HTML baseline formatter
    This commit adds the HTML baseline formatter which works the same
    as the the normal HTML formatter except it displays candidate
    issues in the case where Bandit can't determine which is the
    matching issue.
    
    It also spiffs up the HTML report a bit.  Similar enhancements
    to normal HTML reporting are coming in a subsequent patch.
    
    Change-Id: Ic4fa9b81cea094e62850a9fefffba8e966145695

---
+++ bandit/formatters/html_baseline.py
@@ -0,0 +1,235 @@
+# Copyright (c) 2015 Hewlett Packard Enterprise
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+import logging
+
+from bandit.core import utils
+
+logger = logging.getLogger(__name__)
+
+
+def report(manager, filename, sev_level, conf_level, lines=-1,
+           out_format='html'):
+    """Writes issues to 'filename' in HTML format
+
+    :param manager: the bandit manager object
+    :param filename: output file name
+    :param sev_level: Filtering severity level
+    :param conf_level: Filtering confidence level
+    :param lines: Number of lines to report, -1 for all
+    :param out_format: The output format name
+    """
+
+    header_block = """
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>
+    Bandit Report
+</title>
+
+<style>
+
+html * {
+    font-family: "Arial", sans-serif;
+}
+
+pre {
+    font-family: "Monaco", monospace;
+}
+
+.bordered-box {
+    border: 1px solid black;
+    padding-top:.5em;
+    padding-bottom:.5em;
+    padding-left:1em;
+
+}
+
+.metrics-box {
+    font-size: 1.1em;
+    line-height: 130%;
+}
+
+.metrics-title {
+    font-size: 1.5em;
+    font-weight: 500;
+    margin-bottom: .25em;
+}
+
+.issue-description {
+    font-size: 1.3em;
+    font-weight: 500;
+}
+
+.candidate-issues {
+    margin-left: 2em;
+    border-left: solid 1px; LightGray;
+    padding-left: 5%;
+    margin-top: .2em;
+    margin-bottom: .2em;
+}
+
+.issue-block {
+    border: 1px solid LightGray;
+    padding-left: .5em;
+    padding-top: .5em;
+    padding-bottom: .5em;
+    margin-bottom: .5em;
+}
+
+.issue-sev-high {
+    background-color: Pink;
+}
+
+.issue-sev-medium {
+    background-color: NavajoWhite;
+}
+
+.issue-sev-low {
+    background-color: LightCyan;
+}
+
+</style>
+</head>
+    """
+
+    report_block = """
+<body>
+{metrics}
+{skipped}
+
+<br>
+<span id='results'>
+    {results}
+</span>
+
+</body>
+</html>
+    """
+
+    issue_block = """
+<span id='issue-{issue_no}'>
+<div class='issue-block {issue_class}'>
+    <b>{test_name}: </b> {test_text}<br>
+    <b>Severity: </b>{severity}<br />
+    <b>Confidence: </b>{confidence}</br />
+    <b>File: </b><a href='{path}' target='_blank'>{path}</a> <br />
+{code}
+{candidates}
+</div>
+</span>
+    """
+
+    code_block = """
+<span id='code'>
+<pre>
+{code}
+</pre>
+</span>"""
+
+    candidate_block = """
+<span id='candidates'>
+<br>
+<b>Candidates: </b>
+{candidate_list}
+</span>
+    """
+
+    candidate_issue = """
+<span id='candidate'>
+<div class='candidate-issues'>
+<pre>{code}</pre>
+</div>
+</span>
+"""
+
+    skipped_block = """
+<br>
+<span id='skipped'>
+<div class='bordered-box'>
+<b>Skipped files:</b><br><br>
+{files_list}
+</div>
+</span>
+"""
+
+    metrics_block = """
+<span id='metrics'>
+    <div class='metrics-box bordered-box'>
+        <div class='metrics-title'>
+            Metrics:<br>
+        </div>
+        Total lines of code: <span id='loc'>{loc}</span><br>
+        Total lines skipped (#nosec): <span id='nosec'>{nosec}</span>
+    </div>
+</span>
+
+"""
+
+    issues = manager.get_issue_list(sev_level=sev_level, conf_level=conf_level)
+
+    # build the skipped string to insert in the report
+    skipped_str = ''.join('%s - %s\n' % (fname, reason)
+                          for fname, reason in manager.skipped)
+    if skipped_str:
+        skipped_text = skipped_block.format(files_list=skipped_str)
+    else:
+        skipped_text = ''
+
+    # build the results string to insert in the report
+    results_str = ''
+    for index, issue in enumerate(issues):
+        if len(issues[issue]) == 1:
+            candidates = ''
+            code = code_block.format(code=issue.get_code(lines, True).
+                                     strip('\n').lstrip(' '))
+        else:
+            candidates_str = ''
+            code = ''
+            for candidate in issues[issue]:
+                candidate_code = (candidate.get_code(lines, True).strip('\n').
+                                  lstrip(' '))
+                candidates_str += candidate_issue.format(code=candidate_code)
+
+            candidates = candidate_block.format(candidate_list=candidates_str)
+
+        results_str += issue_block.format(issue_no=index,
+                                          issue_class='issue-sev-{}'.
+                                          format(issue.severity.lower()),
+
+                                          test_name=issue.test,
+                                          test_text=issue.text,
+                                          severity=issue.severity,
+                                          confidence=issue.confidence,
+                                          path=issue.fname, code=code,
+                                          candidates=candidates)
+
+    # build the metrics string to insert in the report
+    metrics_summary = metrics_block.format(
+        loc=manager.metrics.data['_totals']['loc'],
+        nosec=manager.metrics.data['_totals']['nosec'])
+
+    # build the report and output it
+    report_contents = report_block.format(metrics=metrics_summary,
+                                          skipped=skipped_text,
+                                          results=results_str)
+
+    with utils.output_file(filename, 'w') as fout:
+        fout.write(header_block)
+        fout.write(report_contents)
+
+    if filename is not None:
+        logger.info("HTML output written to file: %s" % filename)
---
+++ tests/unit/formatters/test_html_baseline.py
@@ -0,0 +1,139 @@
+# Copyright (c) 2015 Hewlett Packard Enterprise
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+from collections import OrderedDict
+import os
+import tempfile
+
+from bs4 import BeautifulSoup
+import mock
+import testtools
+
+import bandit
+from bandit.core import config
+from bandit.core import constants
+from bandit.core import manager
+from bandit.core import metrics
+from bandit.core import issue
+from bandit.formatters import html_baseline as b_html
+
+
+class BaselineHtmlFormatterTests(testtools.TestCase):
+
+    def setUp(self):
+        super(BaselineHtmlFormatterTests, self).setUp()
+
+        cfg_file = os.path.join(os.getcwd(), 'bandit/config/bandit.yaml')
+        conf = config.BanditConfig(cfg_file)
+        self.manager = manager.BanditManager(conf, 'file')
+
+        (tmp_fd, self.tmp_fname) = tempfile.mkstemp()
+
+        self.manager.out_file = self.tmp_fname
+
+    def test_report_with_skipped(self):
+        self.manager.skipped = [('abc.py', 'File is bad')]
+
+        b_html.report(
+            self.manager, self.tmp_fname, bandit.LOW, bandit.LOW)
+
+        with open(self.tmp_fname) as f:
+            soup = BeautifulSoup(f.read(), 'html.parser')
+            skipped_span = soup.find_all('span', id='skipped')[0]
+
+            self.assertEqual(1, len(soup.find_all('span', id='skipped')))
+            self.assertIn('abc.py', skipped_span.text)
+            self.assertIn('File is bad', skipped_span.text)
+
+    @mock.patch('bandit.core.issue.Issue.get_code')
+    @mock.patch('bandit.core.manager.BanditManager.get_issue_list')
+    def test_report_contents(self, get_issue_list, get_code):
+        self.manager.metrics.data['_totals'] = {'loc': 1000, 'nosec': 50}
+
+        issue_a = _get_issue_instance(severity=bandit.LOW)
+        issue_a.fname = 'abc.py'
+        issue_a.test = 'AAAAAAA'
+        issue_a.text = 'BBBBBBB'
+        issue_a.confidence = 'CCCCCCC'
+        # don't need to test severity, it determines the color which we're
+        # testing separately
+
+        issue_b = _get_issue_instance(severity=bandit.MEDIUM)
+        issue_c = _get_issue_instance(severity=bandit.HIGH)
+
+        issue_x = _get_issue_instance()
+        get_code.return_value = 'some code'
+
+        issue_y = _get_issue_instance()
+
+        get_issue_list.return_value = OrderedDict([(issue_a, [issue_x, issue_y]),
+                                                   (issue_b, [issue_x]),
+                                                   (issue_c, [issue_y])])
+
+        b_html.report(
+            self.manager, self.tmp_fname, bandit.LOW, bandit.LOW)
+
+        with open(self.tmp_fname) as f:
+            soup = BeautifulSoup(f.read(), 'html.parser')
+
+            self.assertEqual('1000', soup.find_all('span', id='loc')[0].text)
+            self.assertEqual('50', soup.find_all('span', id='nosec')[0].text)
+
+            issue1 = soup.find_all('span', id='issue-0')[0]
+            issue2 = soup.find_all('span', id='issue-1')[0]
+            issue3 = soup.find_all('span', id='issue-2')[0]
+
+            # make sure the class has been applied properly
+            self.assertEqual(1, len(issue1.find_all(
+                'div', {'class': 'issue-sev-low'})))
+
+            self.assertEqual(1, len(issue2.find_all(
+                'div', {'class': 'issue-sev-medium'})))
+
+            self.assertEqual(1, len(issue3.find_all(
+                'div', {'class': 'issue-sev-high'})))
+
+            # issue1 has a candidates section with 2 candidates in it
+            self.assertEqual(1, len(issue1.find_all('span', id='candidates')))
+            self.assertEqual(2, len(issue1.find_all('span', id='candidate')))
+
+            # issue2 doesn't have candidates
+            self.assertEqual(0, len(issue2.find_all('span', id='candidates')))
+            self.assertEqual(0, len(issue2.find_all('span', id='candidate')))
+
+            # issue1 doesn't have code issue 2 and 3 do
+            self.assertEqual(0, len(issue1.find_all('span', id='code')))
+            self.assertEqual(1, len(issue2.find_all('span', id='code')))
+            self.assertEqual(1, len(issue3.find_all('span', id='code')))
+
+            # issue2 code and issue1 first candidate have code
+            self.assertIn('some code', issue1.find_all('span',
+                                                       id='candidate')[0].text)
+            self.assertIn('some code', issue2.find_all('span',
+                                                       id='code')[0].text)
+
+            # make sure correct things are being output in issues
+            self.assertIn('AAAAAAA:', issue1.text)
+            self.assertIn('BBBBBBB', issue1.text)
+            self.assertIn('CCCCCCC', issue1.text)
+            self.assertIn('abc.py', issue1.text)
+
+
+
+def _get_issue_instance(severity=bandit.MEDIUM, confidence=bandit.MEDIUM):
+    new_issue = issue.Issue(severity, confidence, 'Test issue')
+    new_issue.fname = 'code.py'
+    new_issue.test = 'bandit_plugin'
+    new_issue.lineno = 1
+    return new_issue
