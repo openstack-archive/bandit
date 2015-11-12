ccae1a20597c1e645bf72c91801a5e718c2bf9ca
Travis McPeak <travis.mcpeak@hp.com>
2015-11-12 09:48:45 +0100

    Update the HTML formatter
    This commit brings the HTML formatter up to date with the style
    improvements introduced in the HTML baseline formatter.
    
    Change-Id: Ife8ae1d3e05d6619fc07393d4a73d92556fe711d

--- bandit/formatters/html.py
+++ bandit/formatters/html.py
@@ -1,16 +1,17 @@
 # Copyright (c) 2015 Rackspace, Inc.
+# Copyright (c) 2015 Hewlett Packard Enterprise
 #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may
-# not use this file except in compliance with the License. You may obtain
-# a copy of the License at
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#       http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations
-# under the License.
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
 
 import logging
 
@@ -21,117 +22,177 @@ logger = logging.getLogger(__name__)
 
 def report(manager, filename, sev_level, conf_level, lines=-1,
            out_format='html'):
-    '''Writes issues to 'filename' in HTML format
+    """Writes issues to 'filename' in HTML format
 
     :param manager: the bandit manager object
     :param filename: output file name
     :param sev_level: Filtering severity level
     :param conf_level: Filtering confidence level
     :param lines: Number of lines to report, -1 for all
-    :param out_format: The ouput format name
-    '''
+    :param out_format: The output format name
+    """
 
-    report_block = """
+    header_block = """
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-.metrics-main {{
-    border: 1px solid black;
-    padding-top:.5em;
-    padding-bottom:.5em;
-    padding-left:1em ;
-    font-size: 1.1em;
-    line-height: 130%;
-}}
 
-.metrics-title {{
-    font-size: 1.5em;
-    font-weight: 500;
-}}
-
-.issue-description {{
-    font-size: 1.3em;
-    font-weight: 500;
-}}
-
-</style>
 <title>
     Bandit Report
 </title>
+
+<style>
+
+html * {
+    font-family: "Arial", sans-serif;
+}
+
+pre {
+    font-family: "Monaco", monospace;
+}
+
+.bordered-box {
+    border: 1px solid black;
+    padding-top:.5em;
+    padding-bottom:.5em;
+    padding-left:1em;
+
+}
+
+.metrics-box {
+    font-size: 1.1em;
+    line-height: 130%;
+}
+
+.metrics-title {
+    font-size: 1.5em;
+    font-weight: 500;
+    margin-bottom: .25em;
+}
+
+.issue-description {
+    font-size: 1.3em;
+    font-weight: 500;
+}
+
+.issue-block {
+    border: 1px solid LightGray;
+    padding-left: .5em;
+    padding-top: .5em;
+    padding-bottom: .5em;
+    margin-bottom: .5em;
+}
+
+.issue-sev-high {
+    background-color: Pink;
+}
+
+.issue-sev-medium {
+    background-color: NavajoWhite;
+}
+
+.issue-sev-low {
+    background-color: LightCyan;
+}
+
+</style>
 </head>
+    """
+
+    report_block = """
 <body>
-    <div class="metrics-main">
-        {metrics}
-    </div>
-    <br><br>
-    <div class="results">
-        {results}
-    </div>
+{metrics}
+{skipped}
+
+<br>
+<span id='results'>
+    {results}
+</span>
+
 </body>
 </html>
-
     """
 
     issue_block = """
-<div class="issue-description"><b>{test_name}:</b> {test_text}</div><br>
-<div class="details">
-    <b>Severity: </b>
-    <span class='severity severity_{severity}'>{severity}</span><br />
-    <b>Confidence:</b>
-    <span class='confidence confidence_{confidence}'>{confidence}</span><br />
-    <b>File:</b>
-    <a class='file_link' href='{path}' target='_blank'>{path}</a> <br />
-</div>
-
-<div class="code">
-    <pre>
+<span id='issue-{issue_no}'>
+<div class='issue-block {issue_class}'>
+    <b>{test_name}: </b> {test_text}<br>
+    <b>Severity: </b>{severity}<br />
+    <b>Confidence: </b>{confidence}</br />
+    <b>File: </b><a href='{path}' target='_blank'>{path}</a> <br />
 {code}
-    </pre>
 </div>
+</span>
     """
 
-    results = {}
+    code_block = """
+<span id='code'>
+<pre>
+{code}
+</pre>
+</span>"""
+
+    skipped_block = """
+<br>
+<span id='skipped'>
+<div class='bordered-box'>
+<b>Skipped files:</b><br><br>
+{files_list}
+</div>
+</span>
+"""
+
+    metrics_block = """
+<span id='metrics'>
+    <div class='metrics-box bordered-box'>
+        <div class='metrics-title'>
+            Metrics:<br>
+        </div>
+        Total lines of code: <span id='loc'>{loc}</span><br>
+        Total lines skipped (#nosec): <span id='nosec'>{nosec}</span>
+    </div>
+</span>
+
+"""
 
     issues = manager.get_issue_list(sev_level=sev_level, conf_level=conf_level)
-    for issue in issues:
-        if not results.get(issue.fname):
-            results[issue.fname] = []
 
-        code = issue.get_code(lines, True)
-        temp_result = issue_block.format(
-            test_name=issue.test,
-            test_text=issue.text,
-            severity=issue.severity,
-            confidence=issue.confidence,
-            path=issue.fname, code=code
-        )
-        results[issue.fname].append(temp_result)
+    # build the skipped string to insert in the report
+    skipped_str = ''.join('%s - %s\n' % (fname, reason)
+                          for fname, reason in manager.skipped)
+    if skipped_str:
+        skipped_text = skipped_block.format(files_list=skipped_str)
+    else:
+        skipped_text = ''
 
-    results_str = ""
-    for res in results:
-        if results[res]:
-            for result in results[res]:
-                results_str += result + "\n"
+    # build the results string to insert in the report
+    results_str = ''
+    for index, issue in enumerate(issues):
+        code = code_block.format(code=issue.get_code(lines, True).
+                                 strip('\n').lstrip(' '))
 
-    # print out basic metrics from run
+        results_str += issue_block.format(issue_no=index,
+                                          issue_class='issue-sev-{}'.
+                                          format(issue.severity.lower()),
 
-    metrics_summary = '<div class=metrics-title>Metrics:</div><br>\n'
+                                          test_name=issue.test,
+                                          test_text=issue.text,
+                                          severity=issue.severity,
+                                          confidence=issue.confidence,
+                                          path=issue.fname, code=code)
 
-    for (label, metric) in [
-        ('Total lines of code', 'loc'),
-        ('Total lines skipped (#nosec)', 'nosec')
-    ]:
-        metrics_summary += "{0}: <span class='{1}'>{2}</span><br>\n".format(
-            label,
-            metric,
-            manager.metrics.data['_totals'][metric]
-        )
+    # build the metrics string to insert in the report
+    metrics_summary = metrics_block.format(
+        loc=manager.metrics.data['_totals']['loc'],
+        nosec=manager.metrics.data['_totals']['nosec'])
 
+    # build the report and output it
     report_contents = report_block.format(metrics=metrics_summary,
+                                          skipped=skipped_text,
                                           results=results_str)
 
     with utils.output_file(filename, 'w') as fout:
+        fout.write(header_block)
         fout.write(report_contents)
 
     if filename is not None:
