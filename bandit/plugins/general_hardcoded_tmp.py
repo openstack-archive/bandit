4dc4d84382b2efe62a2c219c32a246e8b2f05d94
Eric Brown <browne@vmware.com>
2015-12-10 18:00:14 -0800

    Use sphinx autodoc to generate docs from docstring
    Rather than having separate rst documentation files, this patch auto
    generates the docs from the docstrings in the modules. Should
    make it easier to maintain.
    
    Change-Id: Iaed77f8358ccb6edaf2627fbabdcc855272b4ea2

--- bandit/plugins/general_hardcoded_tmp.py
+++ bandit/plugins/general_hardcoded_tmp.py
@@ -14,6 +14,51 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+r"""
+Description
+-----------
+Safely creating a temporary file or directory means following a number of rules
+(see the references for more details). This plugin test looks for strings
+starting with (configurable) commonly used temporary paths, for example:
+
+ - /tmp
+ - /var/tmp
+ - /dev/shm
+ - etc
+
+Available Since
+---------------
+ - Bandit v0.9.0
+
+Config Options
+--------------
+This test plugin takes a similarly named config block, `hardcoded_tmp_directory`.
+The config block provides a Python list, `tmp_dirs`, that lists string fragments
+indicating possible temporary file paths. Any string starting with one of these
+fragments will report a MEDIUM confidence issue.
+
+.. code-block:: yaml
+
+    hardcoded_tmp_directory:
+        tmp_dirs:  ['/tmp', '/var/tmp', '/dev/shm']
+
+
+Sample Output
+-------------
+.. code-block: none
+
+    >> Issue: Probable insecure usage of temp file/directory.
+       Severity: Medium   Confidence: Medium
+       Location: ./examples/hardcoded-tmp.py:1
+    1 f = open('/tmp/abc', 'w')
+    2 f.write('def')
+
+References
+----------
+ - https://security.openstack.org/guidelines/dg_using-temporary-files-securely.html
+
+"""
+
 import bandit
 from bandit.core.test_properties import *
 
