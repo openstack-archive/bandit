83aa9db744490dfc4145b480101b612024ecdbb4
mattvaldes <matthew.valdes@rackspace.com>
2016-01-14 12:02:59 -0600

    Split yaml blacklist check into its own file
    Allow SafeLoader to be passed to the Loader parameter
    Closes-bug: 1508490
    
    Change-Id: I7459577e175b5f2a623316e22c63b2d42ba1db25

---
+++ bandit/plugins/yaml_load.py
@@ -0,0 +1,70 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright 2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+r"""
+Description
+-----------
+Running Flask applications in debug mode results in the Werkzeug debugger
+being enabled. This includes a feature that allows arbitrary code execution.
+Documentation for both Flask [1]_ and Werkzeug [2]_ strongly suggests that
+debug mode should never be enabled on production systems.
+
+Operating a production server with debug mode enabled was the probable cause
+of the Patreon breach in 2015 [3]_.
+
+Config Options
+--------------
+None
+
+Sample Output
+-------------
+.. code-block:: none
+
+    >> Issue: A Flask app appears to be run with debug=True, which exposes
+    the Werkzeug debugger and allows the execution of arbitrary code.
+       Severity: High   Confidence: High
+          Location: examples/flask_debug.py:10
+          9 #bad
+          10    app.run(debug=True)
+          11
+
+References
+----------
+.. [1] http://flask.pocoo.org/docs/0.10/quickstart/#debug-mode
+.. [2] http://werkzeug.pocoo.org/docs/0.10/debug/
+.. [3] http://labs.detectify.com/post/130332638391/how-patreon-got-hacked-publicly-exposed-werkzeug  # noqa
+
+.. versionadded:: 0.15.0
+
+"""
+
+import bandit
+from bandit.core import test_properties as test
+
+
+@test.test_id('B506')
+@test.checks('Call')
+def yaml_load(context):
+    if context.is_module_imported_like('yaml'):
+        if context.call_function_name_qual.endswith('.load'):
+            if not context.check_call_arg_value('Loader', 'SafeLoader'):
+                return bandit.Issue(
+                    severity=bandit.MEDIUM,
+                    confidence=bandit.HIGH,
+                    text="Use of unsafe yaml load. Allows instantiation of"
+                            " arbitrary objects. Consider yaml.safe_load().",
+                    lineno=context.node.lineno,
+                )
