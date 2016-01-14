5f8b20aeb617e2d65a2c6e67b2906fa8c46c065b
Eric Brown <browne@vmware.com>
2016-01-14 08:08:54 -0800

    Enable pep8 testing on tests
    * Add tests directory to the flake8 and linters env
    * Fix up the tests failing pep8
    
    Change-Id: I18446ed58b654fa9829e4de77219792e7b0bbbc4

--- tests/unit/test_bandit_conf_generator.py
+++ tests/unit/test_bandit_conf_generator.py
@@ -1,7 +1,22 @@
+# Copyright (c) 2015 VMware, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+import testtools
+
 from bandit.bandit_config_generator import clean_profile
 from bandit.bandit_config_generator import disable_checkers
 
-import testtools
 
 class BanditConfigGeneratorTests(testtools.TestCase):
     def test_clean_profile(self):
