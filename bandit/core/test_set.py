1a64466745e0b551b96a2ea335560f4b91e2b6ac
Tim Kelsey <tim.kelsey@hp.com>
2016-01-26 16:15:53 +0000

    Old config compatibility
    If bandit finds a config black for 'blacklist_imports' or for
    'blacklist_calls' then it will revert to the old config behaviour.
    This ignores the new blacklist plugin data sources and configures
    the test using the config data instead.
    
    Change-Id: I8c1137efa8df77dbc22fde74af8b876a8b25fbbe

--- bandit/core/test_set.py
+++ bandit/core/test_set.py
@@ -36,7 +36,7 @@ class BanditTestSet():
         filtering = self._get_filter(profile)
         self.plugins = [p for p in extman.plugins
                         if p.plugin._test_id in filtering]
-        self.plugins.extend(self._load_builtins(filtering))
+        self.plugins.extend(self._load_builtins(filtering, config))
         self._load_tests(config, self.plugins)
 
     def _get_filter(self, profile):
@@ -59,7 +59,7 @@ class BanditTestSet():
 
         return set(f for f in filtered if f not in exc)
 
-    def _load_builtins(self, filtering):
+    def _load_builtins(self, filtering, config):
         '''loads up out builtin functions, so they can be filtered.'''
         class Wrapper:
             def __init__(self, name, plugin):
@@ -70,11 +70,12 @@ class BanditTestSet():
 
         if 'B001' in filtering:
             extman = extension_loader.MANAGER
-            blacklist = {}  # filter out blacklist data items
-            for node, tests in six.iteritems(extman.blacklist):
-                values = [t for t in tests if t['id'] in filtering]
-                if values:
-                    blacklist[node] = values
+            blacklist = self._config_compat(config)
+            if not blacklist:  # this is overridden by config, so skip
+                for node, tests in six.iteritems(extman.blacklist):
+                    values = [t for t in tests if t['id'] in filtering]
+                    if values:
+                        blacklist[node] = values
 
             # this dresses up the blacklist to look like a plugin, but the
             # '_checks' data comes from the blacklist information.
@@ -101,6 +102,27 @@ class BanditTestSet():
                 self.tests.setdefault(check, []).append(plugin.plugin)
                 logger.debug('added function %s (%s) targetting %s',
                              plugin.name, plugin.plugin._test_id, check)
+
+    def _config_compat(self, config):
+        '''This reads the old blacklist config and builds compatible data.'''
+        blacklist = {}
+        bl_calls = config.get_setting('blacklist_calls')
+        bl_imports = config.get_setting('blacklist_imports')
+
+        if bl_imports:
+            bl_imports = bl_imports['bad_import_sets']
+            for item in bl_imports:  # patch up the message format
+                item['message'] = item['message'].replace('{module}', '{name}')
+            blacklist.setdefault('Import', []).extend(bl_imports)
+            blacklist.setdefault('ImportFrom', []).extend(bl_imports)
+            blacklist.setdefault('Call', []).extend(bl_imports)
+
+        if bl_calls:
+            bl_calls = bl_calls['bad_name_sets']
+            for item in bl_calls:  # patch up the message format
+                item['message'] = item['message'].replace('{func}', '{name}')
+            blacklist.setdefault('Call', []).extend(bl_calls)
+        return blacklist
 
     def get_tests(self, checktype):
         '''Returns all tests that are of type checktype
