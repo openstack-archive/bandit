bd621bf49cdc6e672704bac0ddfe17ae91c18ac5
Eric Brown <browne@vmware.com>
2016-11-18 22:19:50 -0800

    Trivial fixes based on pylint scan
    * Constants should be in caps
    * Redundant ( ) in if statements
    * Use isinstance instead of type ==
    * Indentation
    
    Change-Id: I79fda14112a9dd02fe867f6d850762216e0ca9a1

--- bandit/plugins/insecure_ssl_tls.py
+++ bandit/plugins/insecure_ssl_tls.py
@@ -117,7 +117,7 @@ def ssl_with_bad_version(context, config):
     .. versionadded:: 0.9.0
     """
     bad_ssl_versions = get_bad_proto_versions(config)
-    if (context.call_function_name_qual == 'ssl.wrap_socket'):
+    if context.call_function_name_qual == 'ssl.wrap_socket':
         if context.check_call_arg_value('ssl_version', bad_ssl_versions):
             return bandit.Issue(
                 severity=bandit.HIGH,
@@ -126,7 +126,7 @@ def ssl_with_bad_version(context, config):
                      "version identified, security issue.",
                 lineno=context.get_lineno_for_call_arg('ssl_version'),
             )
-    elif (context.call_function_name_qual == 'pyOpenSSL.SSL.Context'):
+    elif context.call_function_name_qual == 'pyOpenSSL.SSL.Context':
         if context.check_call_arg_value('method', bad_ssl_versions):
             return bandit.Issue(
                 severity=bandit.HIGH,
@@ -139,7 +139,7 @@ def ssl_with_bad_version(context, config):
     elif (context.call_function_name_qual != 'ssl.wrap_socket' and
           context.call_function_name_qual != 'pyOpenSSL.SSL.Context'):
         if (context.check_call_arg_value('method', bad_ssl_versions) or
-           context.check_call_arg_value('ssl_version', bad_ssl_versions)):
+                context.check_call_arg_value('ssl_version', bad_ssl_versions)):
             lineno = (context.get_lineno_for_call_arg('method') or
                       context.get_lineno_for_call_arg('ssl_version'))
             return bandit.Issue(
@@ -259,7 +259,7 @@ def ssl_with_no_version(context):
 
     .. versionadded:: 0.9.0
     """
-    if (context.call_function_name_qual == 'ssl.wrap_socket'):
+    if context.call_function_name_qual == 'ssl.wrap_socket':
         if context.check_call_arg_value('ssl_version') is None:
             # check_call_arg_value() returns False if the argument is found
             # but does not match the supplied value (or the default None).
