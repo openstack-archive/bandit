cfd9c56bfb5b722fdb930716381bbd630d51a20e
Eric Brown <browne@vmware.com>
2017-02-27 15:44:02 -0800

    Repair the more info links for two blacklist calls
    The blacklist calls has some of documentation anchors combined [1].
    As a result, the links don't correct point to the proper anchor in
    the html. Therefore we need some exception cases for checks that
    have doc combined. Namely B304-B305 and B313-B320.
    
    [1]: https://docs.openstack.org/developer/bandit/blacklists/blacklist_calls.html#b304-b305-ciphers-and-modes
    
    Change-Id: I4dfa905425f2631fa488a9a066c427d4145f4aac

--- bandit/core/docs_utils.py
+++ bandit/core/docs_utils.py
@@ -32,6 +32,13 @@ def get_url(bid):
     if info is not None:
         template = 'blacklists/blacklist_{kind}.html#{id}-{name}'
         if info['id'].startswith('B3'):  # B3XX
+            # Some of the links are combined, so we have exception cases
+            if info['id'] in ['B304', 'B305']:
+                info['id'] = 'b304-b305'
+                info['name'] = 'ciphers-and-modes'
+            elif info['id'] in ['B313', 'B314', 'B315', 'B316', 'B317',
+                                'B318', 'B319', 'B320']:
+                info['id'] = 'b313-b320'
             ext = template.format(
                 kind='calls', id=info['id'], name=info['name'])
         else:
