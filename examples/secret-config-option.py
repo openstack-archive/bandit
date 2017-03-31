a974d1f901fe4e1d1c3b56f8127e666b0bd3ce7b
zhoumo <zhoumo@fiberhome.com>
2017-03-31 10:18:23 +0800

    Use HostAddressOpt for opts that accept IP and hostnames
    Some configuration options were accepting both IP addresses and hostnames.
    Since there was no specific OSLO opt type to support this, we were using
    ``StrOpt``. The change [1] that added support for ``HostAddressOpt`` type
    was merged in Ocata and became available for use with oslo version 3.22.
    
    This patch changes the opt type of configuration options to use this more
    relevant opt type - HostAddressOpt.
    
    [1] I77bdb64b7e6e56ce761d76696bc4448a9bd325eb
    
    Change-Id: If7a62b980ada030e40b3397e621ffd1a9671ab82

--- examples/secret-config-option.py
+++ examples/secret-config-option.py
@@ -4,25 +4,25 @@ from oslo_config import cfg
 # Correct
 secret = True
 opts = [
-    cfg.StrOpt('admin_user',
-               help="User's name"),
-    cfg.StrOpt('admin_password',
-               secret=True,
-               help="User's password"),
-    cfg.StrOpt('nova_password',
-               secret=secret,
-               help="Nova user password"),
+    cfg.HostAddressOpt('admin_user',
+                       help="User's name"),
+    cfg.HostAddressOpt('admin_password',
+                       secret=True,
+                       help="User's password"),
+    cfg.HostAddressOpt('nova_password',
+                       secret=secret,
+                       help="Nova user password"),
 ]
 
 # Incorrect: password not marked secret
 ldap_opts = [
-    cfg.StrOpt('ldap_user',
-               help="LDAP bind user name"),
-    cfg.StrOpt('ldap_password',
-               help="LDAP bind user password"),
-    cfg.StrOpt('ldap_password_attribute',
-               help="LDAP password attribute (default userPassword"),
-    cfg.StrOpt('user_password',
-               secret=False,
-               help="User password"),
+    cfg.HostAddressOpt('ldap_user',
+                       help="LDAP bind user name"),
+    cfg.HostAddressOpt('ldap_password',
+                       help="LDAP bind user password"),
+    cfg.HostAddressOpt('ldap_password_attribute',
+                       help="LDAP password attribute (default userPassword"),
+    cfg.HostAddressOpt('user_password',
+                       secret=False,
+                       help="User password"),
 ]
