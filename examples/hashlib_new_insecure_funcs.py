129568f5b5a929aea721896bfec8facf8a16f080
Rajath Agasthya <rajathagasthya@gmail.com>
2017-09-15 18:25:55 -0700

    Plugin to flag insecure hash functions created using hashlib.new()
    Currently, insecure hash function usage by calling hashlib.md5()
    is flagged in B303. But these hash functions can also be obtained using
    hashlib.new(), by passing 'md4' or 'md5' as an argument. This plugin
    checks such usage. This also retains the same test ID B303 since it is
    essentially checking the same thing.
    
    Change-Id: I8d368aea287e1287e5f638b48c4297d355037839
    Closes-Bug: #1708582

---
+++ examples/hashlib_new_insecure_funcs.py
@@ -0,0 +1,15 @@
+import hashlib
+
+hashlib.new('md5')
+
+hashlib.new('md4', 'test')
+
+hashlib.new(name='md5', string='test')
+
+hashlib.new('MD4', string='test')
+
+hashlib.new(string='test', name='MD5')
+
+hashlib.new('sha256')
+
+hashlib.new('SHA512')
